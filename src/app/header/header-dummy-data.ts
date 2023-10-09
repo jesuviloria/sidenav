2f5f6ace5fc697762b9113f1acad87ad4177dd85
jesuviloria_ <jesudvm12@gmail.com>
2023-10-08 20:39:04 -0500

    commit

---
+++ src/app/header/header-dummy-data.ts
@@ -0,0 +1,59 @@
+export const languages = [
+  {
+    language: 'English',
+    flag: 'us',
+  },
+  {
+    language: 'French',
+    flag: 'france',
+  },
+  {
+    language: 'German',
+    flag: 'germany',
+  },
+  {
+    language: 'Russian',
+    flag: 'russia',
+  },
+  {
+    language: 'Spainsh',
+    flag: 'spain',
+  },
+];
+
+export const notifications = [
+  {
+    icon: 'bi bi-cloud-download',
+    subject: 'Download complete',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  {
+    icon: 'bi bi-cloud-upload',
+    subject: 'Upload complete',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  {
+    icon: 'bi bi-trash',
+    subject: '350 MB trash files',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+];
+
+export const userItems = [
+  {
+    icon: 'bi bi-user',
+    label: 'Profile',
+  },
+  {
+    icon: 'bi bi-cog',
+    label: 'Settings',
+  },
+  {
+    icon: 'bi bi-unlock-alt',
+    label: 'Lock screen',
+  },
+  {
+    icon: 'bi bi-power-off',
+    label: 'Logout',
+  },
+];
